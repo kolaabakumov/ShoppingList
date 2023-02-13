264da7a23b8809513f749ffbfd6676d7f0108f3e
kolaabakumov <kolaabakumov@gmail.com>
2023-02-13 05:22:25 +0300

    Implementation of the data layer

--- app/src/main/java/com/abakumov/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/abakumov/shoppinglist/domain/ShopItem.kt
@@ -3,6 +3,12 @@ package com.abakumov.shoppinglist.domain
 data class ShopItem(
     val name: String,
     val count: Int,
-    val id: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+
+    companion object {
+
+        const val UNDEFINED_ID = -1
+    }
+}
