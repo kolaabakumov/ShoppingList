eb8d9f6a8b0ee9bb1f716387bf2aac3424652ef5
kolaabakumov <kolaabakumov@gmail.com>
2023-02-15 01:35:33 +0300

    Added intents to go into edit mode or add a new ShopItem

--- app/src/main/java/com/abakumov/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/abakumov/shoppinglist/presentation/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.abakumov.shoppinglist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.abakumov.shoppinglist.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
+        }
+        val buttonAddShopItem = findViewById<FloatingActionButton>(R.id.buttonAddShopItem)
+        buttonAddShopItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
 
     }
@@ -65,7 +71,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupClickListener() {
-        shopListAdapter.onShopItemClickListener = { Log.d("MainActivity", it.toString()) }
+        shopListAdapter.onShopItemClickListener = {
+            Log.d("MainActivity", it.toString())
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
+        }
     }
 
     private fun setupLongClickListener() {
