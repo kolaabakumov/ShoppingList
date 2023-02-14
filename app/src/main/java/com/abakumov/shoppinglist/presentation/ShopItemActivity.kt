418f0eab3916b6cd72f13d4ed12eaf6ec6e1f251
kolaabakumov <kolaabakumov@gmail.com>
2023-02-14 08:49:17 +0300

    Create second activity and layout

---
+++ app/src/main/java/com/abakumov/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.abakumov.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abakumov.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
